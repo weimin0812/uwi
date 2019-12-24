ade407cd9a3238ee5e5aabb0164087459b841456
shenxiu <weimin@admindeMacBook-Pro.local>
2019-12-24 16:19:54 +0800

    longest substring without repeating characters

---
+++ src/main/java/leetcode/editor/en/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,67 @@
+//Given a string, find the length of the longest substring without repeating characters. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: "abcabcbb"
+//Output: 3 
+//Explanation: The answer is "abc", with the length of 3. 
+// 
+//
+// 
+// Example 2: 
+//
+// 
+//Input: "bbbbb"
+//Output: 1
+//Explanation: The answer is "b", with the length of 1.
+// 
+//
+// 
+// Example 3: 
+//
+// 
+//Input: "pwwkew"
+//Output: 3
+//Explanation: The answer is "wke", with the length of 3. 
+//             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+// 
+// 
+// 
+// 
+// Related Topics Hash Table Two Pointers String Sliding Window
+package leetcode.editor.en;
+
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+        Solution solution = new LongestSubstringWithoutRepeatingCharacters().new Solution();
+        String s = "abcabcbb";
+        int i = solution.lengthOfLongestSubstring(s);
+        System.out.println(i);
+    }
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            if (s == null || s.length() == 0) {
+                return 0;
+            }
+            int ret = 0, index = 0;
+            HashMap<Character, Integer> map = new HashMap<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                index = Math.max(index, map.getOrDefault(c, 0));
+                ret = Math.max(ret, i - index + 1);
+                map.put(c, i + 1);
+            }
+            return ret;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
