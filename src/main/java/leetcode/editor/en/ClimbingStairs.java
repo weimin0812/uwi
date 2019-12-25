ddf752c7abd9b50e920a6d1f3614bda2019c52d2
shenxiu <weimin@admindeMacBook-Pro.local>
2019-12-25 11:14:09 +0800

    dp easy

---
+++ src/main/java/leetcode/editor/en/ClimbingStairs.java
@@ -0,0 +1,57 @@
+//You are climbing a stair case. It takes n steps to reach to the top. 
+//
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top? 
+//
+// Note: Given n will be a positive integer. 
+//
+// Example 1: 
+//
+// 
+//Input: 2
+//Output: 2
+//Explanation: There are two ways to climb to the top.
+//1. 1 step + 1 step
+//2. 2 steps
+// 
+//
+// Example 2: 
+//
+// 
+//Input: 3
+//Output: 3
+//Explanation: There are three ways to climb to the top.
+//1. 1 step + 1 step + 1 step
+//2. 1 step + 2 steps
+//3. 2 steps + 1 step
+// 
+// Related Topics Dynamic Programming
+package leetcode.editor.en;
+
+public class ClimbingStairs {
+
+    public static void main(String[] args) {
+        Solution solution = new ClimbingStairs().new Solution();
+    }
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int climbStairs(int n) {
+            if (n <= 0) {
+                throw new IllegalArgumentException("n should > 0");
+            }
+            if (n <= 2) {
+                return n;
+            }
+            int f = 1, g = 2;
+            for (int i = 2; i < n; i++) {
+                int t = g;
+                g = f + g;
+                f = t;
+            }
+            return g;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
