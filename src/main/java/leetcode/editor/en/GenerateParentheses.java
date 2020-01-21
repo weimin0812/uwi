4ab132827c575b1ce40315751dc6c85a13373d4c
shenxiu <weimin@admindeMacBook-Pro.local>
2020-01-21 11:54:33 +0800

    generate parenthesis

--- src/main/java/leetcode/editor/en/GenerateParentheses.java
+++ src/main/java/leetcode/editor/en/GenerateParentheses.java
@@ -26,62 +26,29 @@ public class GenerateParentheses {
             System.out.println(s);
         }
     }
-
-
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<String> generateParenthesis(int n) {
-            List<String> list = new ArrayList<>();
-            dfs(0, 0, n, new StringBuffer(), list);
-            return list;
+            List<String> ret = new ArrayList<>();
+            dfs(0, 0, n, new StringBuffer(), ret);
+            return ret;
         }
 
-        private void dfs(int open, int close, int n, StringBuffer sb, List<String> list) {
-            if (sb.length() == 2 * n) {
-                list.add(sb.toString());
-                return;
+        private void dfs(int open, int close, int n, StringBuffer sb, List<String> ret) {
+            if (open == n && close == n) {
+                ret.add(sb.toString());
             }
             if (open < n) {
                 sb.append('(');
-                dfs(open + 1, close, n, sb, list);
+                dfs(open + 1, close, n, sb, ret);
                 sb.deleteCharAt(sb.length() - 1);
             }
             if (close < open) {
                 sb.append(')');
-                dfs(open, close + 1, n, sb, list);
+                dfs(open, close + 1, n, sb, ret);
                 sb.deleteCharAt(sb.length() - 1);
             }
         }
-
-        private void dfs(int i, int n, StringBuffer sb, List<String> list) {
-            if (i == n) {
-                if (valid(sb.toString())) {
-                    list.add(sb.toString());
-                }
-                return;
-            }
-            for (char c : "()".toCharArray()) {
-                sb.append(c);
-                dfs(i + 1, n, sb, list);
-                sb.deleteCharAt(sb.length() - 1);
-            }
-        }
-
-        private boolean valid(String s) {
-            int balance = 0;
-            for (char c : s.toCharArray()) {
-                if (c == ')') {
-                    balance++;
-                } else {
-                    balance--;
-                    if (balance < 0) {
-                        return false;
-                    }
-                }
-            }
-            return balance == 0;
-        }
-
 
     }
 //leetcode submit region end(Prohibit modification and deletion)
