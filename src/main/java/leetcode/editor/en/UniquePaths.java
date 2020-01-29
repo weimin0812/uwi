c1d00a8ae459e04243443fa00782ec6671828661
shenxiu <weimin@admindeMacBook-Pro.local>
2020-01-29 17:25:27 +0800

    unique paths

--- src/main/java/leetcode/editor/en/UniquePaths.java
+++ src/main/java/leetcode/editor/en/UniquePaths.java
@@ -39,17 +39,23 @@ public class UniquePaths {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int uniquePaths(int m, int n) {
-            // dp[i][j]=dp[i][j-1] + dp[i-1][j]
+            // dp[i][j] = dp[i-1][j] + dp[i][j-1]
             int[][] dp = new int[m][n];
-            dp[0][0] = 1;
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (!(i == 0 && j == 0)) {
-                        dp[i][j] = (j - 1 >= 0 ? dp[i][j - 1] : 0) + (i - 1 >= 0 ? dp[i - 1][j] : 0);
+                    if (i == 0 && j == 0) {
+                        dp[i][j] = 1;
+                    } else if (i == 0) {
+                        dp[i][j] = dp[i][j - 1];
+                    } else if (j == 0) {
+                        dp[i][j] = dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
                     }
                 }
             }
             return dp[m - 1][n - 1];
+
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
