add30eb578bf60b084de645f133fbc75d21266af
shenxiu <weimin@admindeMacBook-Pro.local>
2020-01-29 17:35:32 +0800

    minimum path sum

--- src/main/java/leetcode/editor/en/MinimumPathSum.java
+++ src/main/java/leetcode/editor/en/MinimumPathSum.java
@@ -27,21 +27,22 @@ public class MinimumPathSum {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int minPathSum(int[][] grid) {
-            if (grid == null) {
-                return 0;
-            }
-            int r = grid.length, c = grid[0].length;
-            int[][] dp = new int[r][c];
-            for (int i = 0; i < r; i++) {
-                for (int j = 0; j < c; j++) {
+            int[][] dp = new int[grid.length][grid[0].length];
+            // dp[i][j] = grid[i][j] + math.min(dp[i-1][j],dp[i][j-1])
+            for (int i = 0; i < dp.length; i++) {
+                for (int j = 0; j < dp[i].length; j++) {
                     if (i == 0 && j == 0) {
-                        dp[i][j] = grid[0][0];
+                        dp[i][j] = grid[i][j];
+                    } else if (i == 0) {
+                        dp[i][j] = grid[i][j] + dp[i][j - 1];
+                    } else if (j == 0) {
+                        dp[i][j] = grid[i][j] + dp[i - 1][j];
                     } else {
-                        dp[i][j] = grid[i][j] + Math.min((i - 1 >= 0 ? dp[i - 1][j] : Integer.MAX_VALUE), (j - 1 >= 0 ? dp[i][j - 1] : Integer.MAX_VALUE));
+                        dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
                     }
                 }
             }
-            return dp[r - 1][c - 1];
+            return dp[dp.length - 1][dp[0].length - 1];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
