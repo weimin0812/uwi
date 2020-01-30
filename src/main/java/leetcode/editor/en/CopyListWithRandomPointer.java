0d4efab56988f295d1560e1f55770cad0e430473
shenxiu <weimin@admindeMacBook-Pro.local>
2020-01-30 17:42:56 +0800

    copy list with random pointer

---
+++ src/main/java/leetcode/editor/en/CopyListWithRandomPointer.java
@@ -0,0 +1,108 @@
+//A linked list is given such that each node contains an additional random point
+//er which could point to any node in the list or null. 
+//
+// Return a deep copy of the list. 
+//
+// The Linked List is represented in the input/output as a list of n nodes. Each
+// node is represented as a pair of [val, random_index] where: 
+//
+// 
+// val: an integer representing Node.val 
+// random_index: the index of the node (range from 0 to n-1) where random pointe
+//r points to, or null if it does not point to any node. 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+//Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [[1,1],[2,1]]
+//Output: [[1,1],[2,1]]
+// 
+//
+// Example 3: 
+//
+// 
+//
+// 
+//Input: head = [[3,null],[3,0],[3,null]]
+//Output: [[3,null],[3,0],[3,null]]
+// 
+//
+// Example 4: 
+//
+// 
+//Input: head = []
+//Output: []
+//Explanation: Given linked list is empty (null pointer), so return null.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// -10000 <= Node.val <= 10000 
+// Node.random is null or pointing to a node in the linked list. 
+// Number of Nodes will not exceed 1000. 
+// 
+// Related Topics Hash Table Linked List
+
+package leetcode.editor.en;
+
+import leetcode.common.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Phaser;
+
+public class CopyListWithRandomPointer {
+
+    public static void main(String[] args) {
+        Solution solution = new CopyListWithRandomPointer().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+    class Solution {
+        public Node copyRandomList(Node head) {
+            Node current = head;
+            if (current == null) {
+                return null;
+            }
+            Map<Node, Node> map = new HashMap<>();
+            while (current != null) {
+                map.put(current, new Node(current.val));
+                current = current.next;
+            }
+            current = head;
+            while (current != null) {
+                map.get(current).next = map.get(current.next);
+                map.get(current).random = map.get(current.random);
+                current = current.next;
+            }
+            return map.get(head);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
