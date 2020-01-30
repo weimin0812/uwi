351a96d33367a53ac25ac626ffcaa63c62dd1362
shenxiu <weimin@admindeMacBook-Pro.local>
2020-01-30 16:37:59 +0800

    binary tree maximum path sum

--- src/main/java/leetcode/editor/en/BinaryTreeMaximumPathSum.java
+++ src/main/java/leetcode/editor/en/BinaryTreeMaximumPathSum.java
@@ -33,27 +33,44 @@
 
 package leetcode.editor.en;
 
+import leetcode.common.TreeNode;
+
 public class BinaryTreeMaximumPathSum {
 
- public static void main(String[] args) {
+    public static void main(String[] args) {
         Solution solution = new BinaryTreeMaximumPathSum().new Solution();
- }
+    }
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public int maxPathSum(TreeNode root) {
-        
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        private int max = Integer.MIN_VALUE;
+
+        public int maxPathSum(TreeNode root) {
+            max = Integer.MIN_VALUE;
+            helper(root);
+            return max;
+        }
+
+        private int helper(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int left = Math.max(0, helper(root.left));
+            int right = Math.max(0, helper(root.right));
+            max = Math.max(max, root.val + left + right);
+            return root.val + Math.max(left, right);
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
