4af347f9adbb2b428deffacee2cc46c988070c2a
shenxiu <weimin@admindeMacBook-Pro.local>
2020-01-30 11:44:55 +0800

    symmetric tree

--- src/main/java/leetcode/editor/en/SymmetricTree.java
+++ src/main/java/leetcode/editor/en/SymmetricTree.java
@@ -1,4 +1,5 @@
-//Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center). 
+//Given a binary tree, check whether it is a mirror of itself (ie, symmetric aro
+//und its center). 
 //
 // For example, this binary tree [1,2,2,3,4,4,3] is symmetric: 
 //
@@ -27,46 +28,57 @@
 // Note: 
 //Bonus points if you could solve it both recursively and iteratively. 
 // Related Topics Tree Depth-first Search Breadth-first Search
+
 package leetcode.editor.en;
 
-import swordoffer.common.TreeNode;
+import leetcode.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class SymmetricTree {
 
- public static void main(String[] args) {
+    public static void main(String[] args) {
         Solution solution = new SymmetricTree().new Solution();
- }
-
-
+    }
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        if (root == null) {
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public boolean isSymmetric(TreeNode root) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                TreeNode t1 = queue.poll();
+                TreeNode t2 = queue.poll();
+                if (t1 == null || t2 == null) {
+                    if (t1 == t2) {
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+                if (t1.val != t2.val) {
+                    return false;
+                }
+                queue.offer(t1.left);
+                queue.offer(t2.right);
+                queue.offer(t1.right);
+                queue.offer(t2.left);
+            }
             return true;
         }
-        return isSymmetric(root.left, root.right);
-        
     }
-
-    private boolean isSymmetric(TreeNode left, TreeNode right) {
-        if (left == null || right == null) {
-            return left == right;
-        }
-
-        return left.val == right.val && isSymmetric(left.left, right.right)
-                && isSymmetric(left.right, right.left);
-    }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
