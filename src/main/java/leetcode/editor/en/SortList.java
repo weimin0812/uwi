ad0589b8004b08c80b68b47e5c40d044b6352a5a
shenxiu <weimin@admindeMacBook-Pro.local>
2020-01-31 11:02:29 +0800

    sort list

--- src/main/java/leetcode/editor/en/SortList.java
+++ src/main/java/leetcode/editor/en/SortList.java
@@ -17,27 +17,65 @@
 package leetcode.editor.en;
 
 import leetcode.common.ListNode;
+import leetcode.common.Node;
 
 public class SortList {
 
- public static void main(String[] args) {
+    public static void main(String[] args) {
         Solution solution = new SortList().new Solution();
- }
+    }
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public ListNode sortList(ListNode head) {
-        
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode sortList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode fast = head, slow = head, prev = null;
+            while (fast != null && fast.next != null) {
+                prev = slow;
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            prev.next = null;
+            ListNode l1 = sortList(head);
+            ListNode l2 = sortList(slow);
+            return merge(l1, l2);
+        }
+
+        private ListNode merge(ListNode l1, ListNode l2) {
+            ListNode ret = new ListNode(0);
+            ListNode current = ret;
+            while (l1 != null && l2 != null) {
+                if (l1.val <= l2.val) {
+                    current.next = l1;
+                    current = current.next;
+                    l1 = l1.next;
+                } else {
+                    current.next = l2;
+                    current = current.next;
+                    l2 = l2.next;
+                }
+            }
+            if (l1 != null) {
+                current.next = l1;
+            }
+            if (l2 != null) {
+                current.next = l2;
+            }
+            return ret.next;
+        }
+
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
