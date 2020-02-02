e15014104febb6381df9e2bd9f76fc2671aa5de5
shenxiu <weimin@admindeMacBook-Pro.local>
2020-02-02 12:17:38 +0800

    maximal square

--- src/main/java/leetcode/editor/en/MaximalSquare.java
+++ src/main/java/leetcode/editor/en/MaximalSquare.java
@@ -24,25 +24,26 @@ public class MaximalSquare {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int maximalSquare(char[][] matrix) {
+            // if matrix[i][j] == 1
+            //dp[i][j] = Math.min(dp[i-1,j],dp[i,j-1],dp[i-1][j-1])+1
             if (matrix == null || matrix.length == 0) {
                 return 0;
             }
             int[][] dp = new int[matrix.length][matrix[0].length];
-            // dp[i][j] = matrix[i][j] == '1'?0:Math.min(dp[i-1][j],dp[i-1][j-1],dp[i][j-1])+1
             int ret = 0;
             for (int i = 0; i < dp.length; i++) {
                 for (int j = 0; j < dp[i].length; j++) {
-                    if (i == 0) {
-                        dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
-                    } else if (j == 0) {
-                        dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
-                    } else {
-                        dp[i][j] = matrix[i][j] == '1' ? Math.min(Math.min(dp[i - 1][j], dp[i - 1][j - 1]), dp[i][j - 1]) + 1 : 0;
+                    if (matrix[i][j] == '1') {
+                        if (i == 0 || j == 0) {
+                            dp[i][j] = 1;
+                        } else {
+                            dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i - 1][j - 1]), dp[i][j - 1]) + 1;
+                        }
                     }
                     ret = Math.max(ret, dp[i][j]);
                 }
             }
-            return ret*ret;
+            return ret * ret;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
