215b6ae5342283eab46c4d7e64d68cfd5442b430
shenxiu <weimin@admindeMacBook-Pro.local>
2020-02-03 17:51:20 +0800

    longest increasing subsequence

---
+++ src/main/java/leetcode/editor/en/LongestIncreasingSubsequence.java
@@ -0,0 +1,55 @@
+//Given an unsorted array of integers, find the length of longest increasing sub
+//sequence. 
+//
+// Example: 
+//
+// 
+//Input: [10,9,2,5,3,7,101,18]
+//Output: 4 
+//Explanation: The longest increasing subsequence is [2,3,7,101], therefore the 
+//length is 4. 
+//
+// Note: 
+//
+// 
+// There may be more than one LIS combination, it is only necessary for you to r
+//eturn the length. 
+// Your algorithm should run in O(n2) complexity. 
+// 
+//
+// Follow up: Could you improve it to O(n log n) time complexity? 
+// Related Topics Binary Search Dynamic Programming
+
+package leetcode.editor.en;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+
+    public static void main(String[] args) {
+        Solution solution = new LongestIncreasingSubsequence().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int lengthOfLIS(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            int[] dp = new int[nums.length];
+            Arrays.fill(dp, 1);
+            int ret = 1;
+            for (int i = 1; i < dp.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] > nums[j]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+                ret = Math.max(ret, dp[i]);
+            }
+            return ret;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
