7f59b7da6a607629166ac8f42087ed29b9377521
shenxiu <weimin@admindeMacBook-Pro.local>
2020-02-03 12:40:38 +0800

    perfect square

---
+++ src/main/java/leetcode/editor/en/PerfectSquares.java
@@ -0,0 +1,46 @@
+//Given a positive integer n, find the least number of perfect square numbers (f
+//or example, 1, 4, 9, 16, ...) which sum to n. 
+//
+// Example 1: 
+//
+// 
+//Input: n = 12
+//Output: 3 
+//Explanation: 12 = 4 + 4 + 4. 
+//
+// Example 2: 
+//
+// 
+//Input: n = 13
+//Output: 2
+//Explanation: 13 = 4 + 9. Related Topics Math Dynamic Programming Breadth-first
+// Search
+
+package leetcode.editor.en;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PerfectSquares {
+
+    public static void main(String[] args) {
+        Solution solution = new PerfectSquares().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numSquares(int n) {
+            int[] dp = new int[n + 1];
+            Arrays.fill(dp, n);
+            dp[0] = 0;
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j * j <= i; j++) {
+                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                }
+            }
+            return dp[n];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
