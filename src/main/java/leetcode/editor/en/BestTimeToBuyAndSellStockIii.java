a2d43d63ca0e1aad9028afa92941ba5be4d83081
shenxiu <weimin@admindeMacBook-Pro.local>
2020-02-06 16:36:03 +0800

    best t ime to buy and sell stock III

---
+++ src/main/java/leetcode/editor/en/BestTimeToBuyAndSellStockIii.java
@@ -0,0 +1,68 @@
+//Say you have an array for which the ith element is the price of a given stock 
+//on day i. 
+//
+// Design an algorithm to find the maximum profit. You may complete at most two 
+//transactions. 
+//
+// Note: You may not engage in multiple transactions at the same time (i.e., you
+// must sell the stock before you buy again). 
+//
+// Example 1: 
+//
+// 
+//Input: [3,3,5,0,0,3,1,4]
+//Output: 6
+//Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 
+//3-0 = 3.
+//             Then buy on day 7 (price = 1) and sell on day 8 (price = 4), prof
+//it = 4-1 = 3. 
+//
+// Example 2: 
+//
+// 
+//Input: [1,2,3,4,5]
+//Output: 4
+//Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 
+//5-1 = 4.
+//             Note that you cannot buy on day 1, buy on day 2 and sell them lat
+//er, as you are
+//             engaging multiple transactions at the same time. You must sell be
+//fore buying again.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: [7,6,4,3,1]
+//Output: 0
+//Explanation: In this case, no transaction is done, i.e. max profit = 0. 
+// Related Topics Array Dynamic Programming
+
+package leetcode.editor.en;
+
+public class BestTimeToBuyAndSellStockIii {
+
+    public static void main(String[] args) {
+        Solution solution = new BestTimeToBuyAndSellStockIii().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxProfit(int[] prices) {
+            if (prices == null || prices.length == 0) {
+                return 0;
+            }
+            int n = prices.length;
+            int[][][] dp = new int[n][3][2];
+            for (int i = 0; i < n; i++) {
+                for (int k = 1; k <= 2; k++) {
+                    dp[i][k][0] = Math.max(i - 1 >= 0 ? dp[i - 1][k][0] : 0, (i - 1 >= 0 ? dp[i - 1][k][1] : Integer.MIN_VALUE) + prices[i]);
+                    dp[i][k][1] = Math.max(i - 1 >= 0 ? dp[i - 1][k][1] : Integer.MIN_VALUE, ((i - 1 >= 0 && k - 1 > 0) ? dp[i - 1][k - 1][0] : 0) - prices[i]);
+                }
+            }
+            return dp[n - 1][2][0];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/leetcode/editor/en/BestTimeToBuyAndSellStockIv.java
@@ -0,0 +1,48 @@
+//Say you have an array for which the i-th element is the price of a given stock
+// on day i. 
+//
+// Design an algorithm to find the maximum profit. You may complete at most k tr
+//ansactions. 
+//
+// Note: 
+//You may not engage in multiple transactions at the same time (ie, you must sel
+//l the stock before you buy again). 
+//
+// Example 1: 
+//
+// 
+//Input: [2,4,1], k = 2
+//Output: 2
+//Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 
+//4-2 = 2.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: [3,2,6,5,0,3], k = 2
+//Output: 7
+//Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 
+//6-2 = 4.
+//             Then buy on day 5 (price = 0) and sell on day 6 (price = 3), prof
+//it = 3-0 = 3.
+// 
+// Related Topics Dynamic Programming
+
+package leetcode.editor.en;
+
+public class BestTimeToBuyAndSellStockIv {
+
+ public static void main(String[] args) {
+        Solution solution = new BestTimeToBuyAndSellStockIv().new Solution();
+ }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
