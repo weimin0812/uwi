2dcca648060d55fb2cc09b5f6a392ba04a1a3dd6
shenxiu <weimin@admindeMBP.lan>
2020-02-15 22:11:17 +0800

    Two Sum

--- src/main/java/leetcode/editor/en/TwoSum.java
+++ src/main/java/leetcode/editor/en/TwoSum.java
@@ -31,10 +31,11 @@ public class TwoSum {
             }
             Map<Integer, Integer> map = new HashMap<>();
             for (int i = 0; i < nums.length; i++) {
-                if (map.containsKey(nums[i])) {
-                    return new int[]{map.get(nums[i]), i};
+                int n = nums[i];
+                if (map.containsKey(target - n)) {
+                    return new int[]{map.get(target - n), i};
                 }
-                map.put(target - nums[i], i);
+                map.put(n, i);
             }
             return new int[]{};
         }
--- src/main/java/swordoffer/question32/Solution.java
+++ src/main/java/swordoffer/question32/Solution.java
@@ -1,6 +1,7 @@
 package swordoffer.question32;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
