58f436e36629200117454e49a825a0469ae39c37
shenxiu <weimin@admindeMBP.lan>
2020-02-15 22:23:29 +0800

    ThreeSum

--- src/main/java/leetcode/editor/en/ThreeSum.java
+++ src/main/java/leetcode/editor/en/ThreeSum.java
@@ -1,4 +1,6 @@
-//Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero. 
+//Given an array nums of n integers, are there elements a, b, c in nums such tha
+//t a + b + c = 0? Find all unique triplets in the array which gives the sum of ze
+//ro. 
 //
 // Note: 
 //
@@ -15,9 +17,12 @@
 //  [-1, -1, 2]
 //]
 // Related Topics Array Two Pointers
+
 package leetcode.editor.en;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ThreeSum {
 
@@ -28,16 +33,20 @@ public class ThreeSum {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
-            Arrays.sort(nums);
             List<List<Integer>> ret = new ArrayList<>();
+            if (nums == null || nums.length < 3) {
+                return ret;
+            }
+            Arrays.sort(nums);
             for (int i = 0; i < nums.length; i++) {
                 if (i != 0 && nums[i] == nums[i - 1]) {
                     continue;
                 }
                 int l = i + 1, h = nums.length - 1;
+                int target = -nums[i];
                 while (l < h) {
-                    int sum = nums[i] + nums[l] + nums[h];
-                    if (sum == 0) {
+                    int sum = nums[l] + nums[h];
+                    if (sum == target) {
                         ret.add(Arrays.asList(nums[i], nums[l], nums[h]));
                         l++;
                         h--;
@@ -47,10 +56,10 @@ public class ThreeSum {
                         while (l < h && nums[h] == nums[h + 1]) {
                             h--;
                         }
-                    } else if (sum > 0) {
-                        h--;
-                    } else {
+                    } else if (sum < target) {
                         l++;
+                    } else {
+                        h--;
                     }
                 }
             }
