beef3896c81319d345d27ff5e791b8dbd298f838
shenxiu <weimin@admindeMacBook-Pro.local>
2020-02-17 21:42:53 +0800

    container with most water

--- src/main/java/leetcode/editor/en/ContainerWithMostWater.java
+++ src/main/java/leetcode/editor/en/ContainerWithMostWater.java
@@ -1,4 +1,7 @@
-//Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water. 
+//Given n non-negative integers a1, a2, ..., an , where each represents a point 
+//at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of
+// line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis for
+//ms a container, such that the container contains the most water. 
 //
 // Note: You may not slant the container and n is at least 2. 
 //
@@ -6,7 +9,8 @@
 //
 // 
 //
-// The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49. 
+// The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In thi
+//s case, the max area of water (blue section) the container can contain is 49. 
 //
 // 
 //
@@ -15,6 +19,7 @@
 // 
 //Input: [1,8,6,2,5,4,8,3,7]
 //Output: 49 Related Topics Array Two Pointers
+
 package leetcode.editor.en;
 
 public class ContainerWithMostWater {
@@ -23,18 +28,19 @@ public class ContainerWithMostWater {
         Solution solution = new ContainerWithMostWater().new Solution();
     }
 
-
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int maxArea(int[] height) {
-            int ret = 0, l = 0, r = height.length - 1;
-            while (l <= r) {
-                int h = Math.min(height[l], height[r]);
-                ret = Math.max(ret, (r - l) * h);
-                if (height[l] <= height[r]) {
+            if (height == null || height.length <= 1) {
+                return 0;
+            }
+            int l = 0, h = height.length - 1, ret = Math.min(height[l], height[h]) * (h - l);
+            while (l < h) {
+                ret = Math.max(ret, Math.min(height[l], height[h]) * (h - l));
+                if (height[l] <= height[h]) {
                     l++;
                 } else {
-                    r--;
+                    h--;
                 }
             }
             return ret;
