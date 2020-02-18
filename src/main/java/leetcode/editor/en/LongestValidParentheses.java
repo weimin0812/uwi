0e34485a06e3dcf46a9785823c1226eb7f8ee725
shenxiu <weimin@admindeMacBook-Pro.local>
2020-02-18 20:34:38 +0800

    longest valid parentheses

--- src/main/java/leetcode/editor/en/LongestValidParentheses.java
+++ src/main/java/leetcode/editor/en/LongestValidParentheses.java
@@ -1,4 +1,5 @@
-//Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring. 
+//Given a string containing just the characters '(' and ')', find the length of 
+//the longest valid (well-formed) parentheses substring. 
 //
 // Example 1: 
 //
@@ -16,37 +17,37 @@
 //Explanation: The longest valid parentheses substring is "()()"
 // 
 // Related Topics String Dynamic Programming
+
 package leetcode.editor.en;
 
 public class LongestValidParentheses {
 
     public static void main(String[] args) {
         Solution solution = new LongestValidParentheses().new Solution();
-        System.out.println(solution.longestValidParentheses(")()())"));
     }
-
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int longestValidParentheses(String s) {
-            if (s == null || s.isEmpty()) {
+            if (s == null || s.length() == 0) {
                 return 0;
             }
-            int ret = 0;
             int[] dp = new int[s.length()];
-            for (int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
-                if (((i - 1) >= 0) && c == ')') {
+            int ret = 0;
+            for (int i = 0; i < dp.length; i++) {
+                // ((()))
+                // 012345
+                //
+                if (i > 0 && s.charAt(i) == ')') {
                     if (s.charAt(i - 1) == '(') {
                         dp[i] = 2 + (i - 2 >= 0 ? dp[i - 2] : 0);
                     } else {
-                        int index = i - 1 - dp[i - 1];
-                        if (index >= 0 && s.charAt(index) == '(') {
-                            dp[i] = dp[i - 1] + 2 + (index - 1 >= 0 ? dp[index - 1] : 0);
+                        if (i - 1 - dp[i - 1] >= 0 && s.charAt(i - 1 - dp[i - 1]) == '(') {
+                            dp[i] = dp[i - 1] + 2 + (i - 1 - dp[i - 1] - 1 >= 0 ? dp[i - 1 - dp[i - 1] - 1] : 0);
                         }
                     }
-                    ret = Math.max(ret, dp[i]);
                 }
+                ret = Math.max(ret, dp[i]);
             }
             return ret;
         }
