e25ab72f16d7fee1b95d76b9ca38a5b9f7df8c9e
shenxiu <weimin@admindeMacBook-Pro.local>
2020-02-18 20:23:53 +0800

    next permutation

--- src/main/java/leetcode/editor/en/NextPermutation.java
+++ src/main/java/leetcode/editor/en/NextPermutation.java
@@ -1,15 +1,19 @@
-//Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers. 
+//Implement next permutation, which rearranges numbers into the lexicographicall
+//y next greater permutation of numbers. 
 //
-// If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order). 
+// If such arrangement is not possible, it must rearrange it as the lowest possi
+//ble order (ie, sorted in ascending order). 
 //
 // The replacement must be in-place and use only constant extra memory. 
 //
-// Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column. 
+// Here are some examples. Inputs are in the left-hand column and its correspond
+//ing outputs are in the right-hand column. 
 //
 // 1,2,3 → 1,3,2 
 //3,2,1 → 1,2,3 
 //1,1,5 → 1,5,1 
 // Related Topics Array
+
 package leetcode.editor.en;
 
 public class NextPermutation {
@@ -18,27 +22,28 @@ public class NextPermutation {
         Solution solution = new NextPermutation().new Solution();
     }
 
-
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public void nextPermutation(int[] nums) {
-            //next permutation
             if (nums == null || nums.length < 2) {
                 return;
             }
             int i = nums.length - 2;
-            while (i >= 0 && nums[i] >= nums[i + 1]) {
-                i--;
+            for (; i >= 0; i--) {
+                if (nums[i] < nums[i + 1]) {
+                    break;
+                }
             }
             if (i >= 0) {
-                for (int j = nums.length - 1; j > i; j--) {
+                int j = nums.length - 1;
+                for (; j > i; j--) {
                     if (nums[j] > nums[i]) {
                         swap(nums, i, j);
                         break;
                     }
                 }
             }
-            reverse(nums, i + 1);
+            reverse(nums, i + 1, nums.length - 1);
         }
 
         private void swap(int[] nums, int i, int j) {
@@ -47,12 +52,11 @@ public class NextPermutation {
             nums[j] = t;
         }
 
-        private void reverse(int[] nums, int start) {
-            int end = nums.length - 1;
-            while (start < end) {
-                swap(nums, start, end);
-                start++;
-                end--;
+        private void reverse(int[] nums, int l, int h) {
+            while (l < h) {
+                swap(nums, l, h);
+                l++;
+                h--;
             }
         }
 
