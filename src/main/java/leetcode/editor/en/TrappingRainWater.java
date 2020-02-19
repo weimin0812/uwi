f4576f55a5e6467eab828ec6ac657fa0f2cbb3b0
shenxiu <weimin@admindeMBP.lan>
2020-02-19 17:00:10 +0800

    trapping rain water

--- src/main/java/leetcode/editor/en/TrappingRainWater.java
+++ src/main/java/leetcode/editor/en/TrappingRainWater.java
@@ -1,7 +1,10 @@
-//Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining. 
+//Given n non-negative integers representing an elevation map where the width of
+// each bar is 1, compute how much water it is able to trap after raining. 
 //
 // 
-//The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image! 
+//The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In 
+//this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos
+// for contributing this image! 
 //
 // Example: 
 //
@@ -9,6 +12,7 @@
 //Input: [0,1,0,2,1,0,1,3,2,1,2,1]
 //Output: 6 
 // Related Topics Array Two Pointers Stack
+
 package leetcode.editor.en;
 
 public class TrappingRainWater {
@@ -17,26 +21,24 @@ public class TrappingRainWater {
         Solution solution = new TrappingRainWater().new Solution();
     }
 
-
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int trap(int[] height) {
-            int left = 0, right = height.length - 1, leftMax = 0, rightMax = 0, ret = 0;
-            while (left < right) {
-                if (height[left] < height[right]) {
-                    if (height[left] >= leftMax) {
-                        leftMax = height[left];
-                    } else {
-                        ret += (leftMax - height[left]);
-                    }
-                    left++;
+            if (height == null || height.length <= 1) {
+                return 0;
+            }
+            int l = 0, h = height.length - 1;
+            int lMax = height[l], rMax = height[h];
+            int ret = 0;
+            while (l <= h) {
+                lMax = Math.max(lMax, height[l]);
+                rMax = Math.max(rMax, height[h]);
+                if (lMax < rMax) {
+                    ret += (lMax - height[l]);
+                    l++;
                 } else {
-                    if (height[right] >= rightMax) {
-                        rightMax = height[right];
-                    } else {
-                        ret += (rightMax - height[right]);
-                    }
-                    right--;
+                    ret += (rMax - height[h]);
+                    h--;
                 }
             }
             return ret;
