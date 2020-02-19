303b083bb0090b01ddb2a156135d180e7b62adb1
shenxiu <weimin@admindeMBP.lan>
2020-02-19 18:11:06 +0800

    Jump Games

--- src/main/java/leetcode/editor/en/JumpGame.java
+++ src/main/java/leetcode/editor/en/JumpGame.java
@@ -1,6 +1,8 @@
-//Given an array of non-negative integers, you are initially positioned at the first index of the array. 
+//Given an array of non-negative integers, you are initially positioned at the f
+//irst index of the array. 
 //
-// Each element in the array represents your maximum jump length at that position. 
+// Each element in the array represents your maximum jump length at that positio
+//n. 
 //
 // Determine if you are able to reach the last index. 
 //
@@ -18,9 +20,11 @@
 //Input: [3,2,1,0,4]
 //Output: false
 //Explanation: You will always arrive at index 3 no matter what. Its maximum
-//             jump length is 0, which makes it impossible to reach the last index.
+//             jump length is 0, which makes it impossible to reach the last ind
+//ex.
 // 
 // Related Topics Array Greedy
+
 package leetcode.editor.en;
 
 public class JumpGame {
@@ -29,16 +33,18 @@ public class JumpGame {
         Solution solution = new JumpGame().new Solution();
     }
 
-
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public boolean canJump(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return false;
+            }
             int max = 0;
             for (int i = 0; i < nums.length; i++) {
                 if (i > max) {
                     return false;
                 }
-                max = Math.max(i + nums[i], max);
+                max = Math.max(max, i + nums[i]);
             }
             return true;
         }
