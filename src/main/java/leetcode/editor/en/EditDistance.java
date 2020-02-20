c31eb0fee40d161e1ad02b7cbf45ba58a3748c14
shenxiu <weimin@admindeMBP.lan>
2020-02-20 10:50:38 +0800

    edit distance

--- src/main/java/leetcode/editor/en/EditDistance.java
+++ src/main/java/leetcode/editor/en/EditDistance.java
@@ -36,18 +36,39 @@ package leetcode.editor.en;
 
 public class EditDistance {
 
- public static void main(String[] args) {
+    public static void main(String[] args) {
         Solution solution = new EditDistance().new Solution();
- }
-
-
-
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int minDistance(String word1, String word2) {
-        return 0;
     }
-}
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int minDistance(String word1, String word2) {
+            //dp[i][j] word1[0,...,i] and word2[0,...,j]
+            int m = word1.length();
+            int n = word2.length();
+            int[][] dp = new int[m + 1][n + 1];
+            for (int i = 0; i <= m; i++) {
+                dp[i][0] = i;
+            }
+            for (int i = 0; i <= n; i++) {
+                dp[0][i] = i;
+            }
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else {
+                        int a = dp[i - 1][j - 1];
+                        int b = dp[i][j - 1];
+                        int c = dp[i - 1][j];
+                        dp[i][j] = Math.min(Math.min(a, b), c) + 1;
+                    }
+                }
+            }
+            return dp[m][n];
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
