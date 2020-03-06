a85ab2554bc277fb2845e2364238c29f66a9769c
shenxiu <weimin@admindeMacBook-Pro.local>
2020-03-06 11:51:52 +0800

    add two numbers

--- src/main/java/leetcode/editor/en/AddTwoNumbers.java
+++ src/main/java/leetcode/editor/en/AddTwoNumbers.java
@@ -39,14 +39,15 @@ public class AddTwoNumbers {
             ListNode ret = new ListNode(0);
             ListNode current = ret;
             int carry = 0;
-            while (carry != 0 || l1 != null || l2 != null) {
+            while (l1 != null || l2 != null || carry != 0) {
                 int x = l1 == null ? 0 : l1.val;
                 int y = l2 == null ? 0 : l2.val;
-                current.next = new ListNode((x + y + carry) % 10);
+                int sum = x + y + carry;
+                current.next = new ListNode(sum % 10);
+                carry = sum / 10;
                 l1 = l1 == null ? null : l1.next;
                 l2 = l2 == null ? null : l2.next;
                 current = current.next;
-                carry = (x + y + carry) / 10;
             }
             return ret.next;
         }
