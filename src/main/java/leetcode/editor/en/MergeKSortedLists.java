c64214cc07e3d98c8b4d36d519c896e5be1683ea
shenxiu <weimin@admindeMBP.lan>
2020-03-07 21:44:47 +0800

    merge k sorted lists

--- src/main/java/leetcode/editor/en/MergeKSortedLists.java
+++ src/main/java/leetcode/editor/en/MergeKSortedLists.java
@@ -50,16 +50,14 @@ public class MergeKSortedLists {
         }
 
         private ListNode mergeTwo(ListNode l1, ListNode l2) {
-            if (l1 == null) {
-                return l2;
-            }
-            if (l2 == null) {
-                return l1;
+            if (l1 == null || l2 == null) {
+                return l1 == null ? l2 : l1;
             }
             ListNode ret = l1.val <= l2.val ? l1 : l2;
             ret.next = mergeTwo(l1.val <= l2.val ? l1.next : l1, l1.val <= l2.val ? l2 : l2.next);
             return ret;
         }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
