36150a6f542c120576ec65cc62cfcd51713b8efa
shenxiu <weimin@admindeMacBook-Pro.local>
2020-03-08 07:42:15 +0800

    longest valid parentheses

--- src/main/java/leetcode/editor/en/LongestValidParentheses.java
+++ src/main/java/leetcode/editor/en/LongestValidParentheses.java
@@ -29,25 +29,23 @@ public class LongestValidParentheses {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int longestValidParentheses(String s) {
-            if (s == null || s.length() == 0) {
+            if (s == null || s.length() < 2) {
                 return 0;
             }
             int[] dp = new int[s.length()];
             int ret = 0;
-            for (int i = 0; i < dp.length; i++) {
-                // ((()))
-                // 012345
-                //
-                if (i > 0 && s.charAt(i) == ')') {
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) == ')') {
                     if (s.charAt(i - 1) == '(') {
-                        dp[i] = 2 + (i - 2 >= 0 ? dp[i - 2] : 0);
+                        dp[i] = (i - 2 >= 0 ? dp[i - 2] : 0) + 2;
                     } else {
-                        if (i - 1 - dp[i - 1] >= 0 && s.charAt(i - 1 - dp[i - 1]) == '(') {
-                            dp[i] = dp[i - 1] + 2 + (i - 1 - dp[i - 1] - 1 >= 0 ? dp[i - 1 - dp[i - 1] - 1] : 0);
+                        int index = i - 1 - dp[i - 1];
+                        if (index >= 0 && s.charAt(index) == '(') {
+                            dp[i] = dp[i - 1] + (index - 1 >= 0 ? dp[index - 1] : 0) + 2;
                         }
                     }
+                    ret = Math.max(ret, dp[i]);
                 }
-                ret = Math.max(ret, dp[i]);
             }
             return ret;
         }
