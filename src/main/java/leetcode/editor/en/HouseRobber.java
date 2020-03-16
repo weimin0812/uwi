7d83fe20a05ed8ea704ab9d8b7bc14d089a7618a
shenxiu <weimin@admindeMacBook-Pro.local>
2020-03-16 11:38:30 +0800

    house robber

--- src/main/java/leetcode/editor/en/HouseRobber.java
+++ src/main/java/leetcode/editor/en/HouseRobber.java
@@ -1,6 +1,12 @@
-//You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night. 
+//You are a professional robber planning to rob houses along a street. Each hous
+//e has a certain amount of money stashed, the only constraint stopping you from r
+//obbing each of them is that adjacent houses have security system connected and i
+//t will automatically contact the police if two adjacent houses were broken into 
+//on the same night. 
 //
-// Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police. 
+// Given a list of non-negative integers representing the amount of money of eac
+//h house, determine the maximum amount of money you can rob tonight without alert
+//ing the police. 
 //
 // Example 1: 
 //
@@ -15,7 +21,8 @@
 // 
 //Input: [2,7,9,3,1]
 //Output: 12
-//Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+//Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 
+//(money = 1).
 //             Total amount you can rob = 2 + 9 + 1 = 12.
 // 
 // Related Topics Dynamic Programming
@@ -26,7 +33,6 @@ public class HouseRobber {
     public static void main(String[] args) {
         Solution solution = new HouseRobber().new Solution();
     }
-
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
