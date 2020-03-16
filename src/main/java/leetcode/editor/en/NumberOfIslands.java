f55b8d1cbeda82917903ea5e005c2dbfc709ddcb
shenxiu <weimin@admindeMacBook-Pro.local>
2020-03-16 14:55:24 +0800

    number of islands

--- src/main/java/leetcode/editor/en/NumberOfIslands.java
+++ src/main/java/leetcode/editor/en/NumberOfIslands.java
@@ -39,29 +39,26 @@ public class NumberOfIslands {
     class Solution {
         public int numIslands(char[][] grid) {
             int count = 0;
-            if (grid == null) {
-                return count;
-            }
             for (int i = 0; i < grid.length; i++) {
                 for (int j = 0; j < grid[i].length; j++) {
                     if (grid[i][j] == '1') {
-                        dfs(grid, i, j);
                         count++;
+                        dfs(grid, i, j);
                     }
                 }
             }
             return count;
         }
 
-        private void dfs(char[][] g, int i, int j) {
-            if (i < 0 || i >= g.length || j < 0 || j >= g[0].length || g[i][j] != '1') {
+        private void dfs(char[][] grid, int i, int j) {
+            if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
                 return;
             }
-            g[i][j] = '0';
-            dfs(g, i + 1, j);
-            dfs(g, i - 1, j);
-            dfs(g, i, j + 1);
-            dfs(g, i, j - 1);
+            grid[i][j] = '0';
+            dfs(grid, i + 1, j);
+            dfs(grid, i - 1, j);
+            dfs(grid, i, j + 1);
+            dfs(grid, i, j - 1);
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
