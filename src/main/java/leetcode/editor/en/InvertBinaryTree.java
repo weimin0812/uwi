6c15ba5499292eda97db4bc38d61d73add4b7cd4
shenxiu <weimin@admindeMacBook-Pro.local>
2020-03-16 16:10:56 +0800

    invert binery tree

--- src/main/java/leetcode/editor/en/InvertBinaryTree.java
+++ src/main/java/leetcode/editor/en/InvertBinaryTree.java
@@ -53,26 +53,38 @@ public class InvertBinaryTree {
      */
     class Solution {
         public TreeNode invertTree(TreeNode root) {
-            TreeNode current = root;
-            if (current == null) {
-                return null;
+            if (root == null) {
+                return root;
             }
-            Queue<TreeNode> queue = new LinkedList<>();
-            queue.offer(current);
-            while (!queue.isEmpty()) {
-                current = queue.poll();
-                TreeNode t = current.left;
-                current.left = current.right;
-                current.right = t;
-                if (current.left != null) {
-                    queue.offer(current.left);
-                }
-                if (current.right != null) {
-                    queue.offer(current.right);
-                }
-            }
+            TreeNode node = root.left;
+            root.left = root.right;
+            root.right = node;
+            invertTree(root.left);
+            invertTree(root.right);
             return root;
         }
+
+//        public TreeNode invertTree(TreeNode root) {
+//            TreeNode current = root;
+//            if (current == null) {
+//                return null;
+//            }
+//            Queue<TreeNode> queue = new LinkedList<>();
+//            queue.offer(current);
+//            while (!queue.isEmpty()) {
+//                current = queue.poll();
+//                TreeNode t = current.left;
+//                current.left = current.right;
+//                current.right = t;
+//                if (current.left != null) {
+//                    queue.offer(current.left);
+//                }
+//                if (current.right != null) {
+//                    queue.offer(current.right);
+//                }
+//            }
+//            return root;
+//        }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
