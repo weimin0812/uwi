ecff1fca9662b5df691cb0fe9589b6de38035297
shenxiu <weimin@admindeMacBook-Pro.local>
2020-03-16 15:05:13 +0800

    course schedule

--- src/main/java/leetcode/editor/en/CourseSchedule.java
+++ src/main/java/leetcode/editor/en/CourseSchedule.java
@@ -1,4 +1,5 @@
-//There are a total of n courses you have to take, labeled from 0 to n-1. 
+//There are a total of numCourses courses you have to take, labeled from 0 to nu
+//mCourses-1. 
 //
 // Some courses may have prerequisites, for example to take course 0 you have to
 // first take course 1, which is expressed as a pair: [0,1] 
@@ -6,19 +7,21 @@
 // Given the total number of courses and a list of prerequisite pairs, is it pos
 //sible for you to finish all courses? 
 //
+// 
 // Example 1: 
 //
 // 
-//Input: 2, [[1,0]] 
+//Input: numCourses = 2, prerequisites = [[1,0]]
 //Output: true
 //Explanation: There are a total of 2 courses to take. 
 //             To take course 1 you should have finished course 0. So it is poss
-//ible. 
+//ible.
+// 
 //
 // Example 2: 
 //
 // 
-//Input: 2, [[1,0],[0,1]]
+//Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
 //Output: false
 //Explanation: There are a total of 2 courses to take. 
 //             To take course 1 you should have finished course 0, and to take c
@@ -26,13 +29,15 @@
 //             also have finished course 1. So it is impossible.
 // 
 //
-// Note: 
+// 
+// Constraints: 
 //
 // 
 // The input prerequisites is a graph represented by a list of edges, not adjace
 //ncy matrices. Read more about how a graph is represented. 
 // You may assume that there are no duplicate edges in the input prerequisites. 
 //
+// 1 <= numCourses <= 10^5 
 // 
 // Related Topics Depth-first Search Breadth-first Search Graph Topological Sort
 //
@@ -51,34 +56,30 @@ public class CourseSchedule {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public boolean canFinish(int numCourses, int[][] prerequisites) {
-            if (numCourses <= 0) {
-                return false;
-            }
             int[] inDegree = new int[numCourses];
             for (int[] pre : prerequisites) {
                 inDegree[pre[0]]++;
             }
-            Queue<Integer> q = new LinkedList<>();
+            Queue<Integer> queue = new LinkedList<>();
             for (int i = 0; i < inDegree.length; i++) {
                 if (inDegree[i] == 0) {
-                    q.offer(i);
+                    queue.offer(i);
                 }
             }
             int count = 0;
-            while (!q.isEmpty()) {
-                int start = q.poll();
+            while (!queue.isEmpty()) {
                 count++;
+                int c = queue.poll();
                 for (int[] pre : prerequisites) {
-                    if (pre[1] == start) {
+                    if (pre[1] == c) {
                         inDegree[pre[0]]--;
                         if (inDegree[pre[0]] == 0) {
-                            q.offer(pre[0]);
+                            queue.offer(pre[0]);
                         }
                     }
                 }
             }
             return count == numCourses;
-
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
