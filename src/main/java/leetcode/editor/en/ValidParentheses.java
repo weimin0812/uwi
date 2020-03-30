39b9843078ea52d1b353d557152c3ec9e5887e48
shenxiu <weimin@admindeMBP.lan>
2020-03-30 23:33:27 +0800

    valid parentheses

--- src/main/java/leetcode/editor/en/ValidParentheses.java
+++ src/main/java/leetcode/editor/en/ValidParentheses.java
@@ -66,14 +66,14 @@ public class ValidParentheses {
             }
             Map<Character, Character> map = new HashMap<>();
             map.put('(', ')');
-            map.put('[', ']');
             map.put('{', '}');
+            map.put('[', ']');
             Stack<Character> stack = new Stack<>();
             for (char c : s.toCharArray()) {
                 if (map.containsKey(c)) {
                     stack.push(map.get(c));
                 } else {
-                    if (stack.isEmpty() || c != stack.pop()) {
+                    if (stack.isEmpty() || !stack.pop().equals(c)) {
                         return false;
                     }
                 }
