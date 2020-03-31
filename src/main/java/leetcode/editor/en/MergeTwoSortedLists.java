daee2c7c8dd597be0484dfc6b19374307cf20f3e
shenxiu <weimin@admindeMacBook-Pro.local>
2020-03-31 15:53:16 +0800

    merge two sorted lists

--- src/main/java/leetcode/editor/en/MergeTwoSortedLists.java
+++ src/main/java/leetcode/editor/en/MergeTwoSortedLists.java
@@ -30,35 +30,26 @@ public class MergeTwoSortedLists {
      */
     class Solution {
         public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-            if (l1 == null || l2 == null) {
-                return l1 == null ? l2 : l1;
+            ListNode ret = new ListNode(0);
+            ListNode current = ret;
+            while (l1 != null && l2 != null) {
+                if (l1.val <= l2.val) {
+                    current.next = l1;
+                    l1 = l1.next;
+                } else {
+                    current.next = l2;
+                    l2 = l2.next;
+                }
+                current = current.next;
             }
-            ListNode ret = l1.val <= l2.val ? l1 : l2;
-            ret.next = mergeTwoLists(l1.val <= l2.val ? l1.next : l1, l1.val <= l2.val ? l2 : l2.next);
-            return ret;
+            if (l1 != null) {
+                current.next = l1;
+            }
+            if (l2 != null) {
+                current.next = l2;
+            }
+            return ret.next;
         }
-
-//        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-//            ListNode ret = new ListNode(0);
-//            ListNode current = ret;
-//            while (l1 != null && l2 != null) {
-//                if (l1.val <= l2.val) {
-//                    current.next = l1;
-//                    l1 = l1.next;
-//                } else {
-//                    current.next = l2;
-//                    l2 = l2.next;
-//                }
-//                current = current.next;
-//            }
-//            if (l1 != null) {
-//                current.next = l1;
-//            }
-//            if (l2 != null) {
-//                current.next = l2;
-//            }
-//            return ret.next;
-//        }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
